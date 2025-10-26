d5e9f3cc933b5a0dd6340ebe26a1ad07afda4328
Biboswan Roy <biboswan98@gmail.com>
2025-10-26 20:44:35 +0530

    Include typescript files dynamically for generate lesson route

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+    outputFileTracingIncludes: {
+      '/api/generate-lesson': ['./node_modules/typescript/lib/**'],
+    },
 };
 
 export default nextConfig;
