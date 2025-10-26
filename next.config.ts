bbaf5797abb4e7e729030df278d4f524d9eaa75d
Biboswan Roy <biboswan98@gmail.com>
2025-10-27 02:31:37 +0530

    explicit adding of typescript libs

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   
-    // outputFileTracingIncludes: {
-    //   '/api/generate-lesson': ['node_modules/typescript/lib/lib.d.ts','node_modules/typescript/lib/lib.esnext.d.ts'],
-    // },
+    outputFileTracingIncludes: {
+      '/api/generate-lesson': ['node_modules/typescript/lib/lib.d.ts','node_modules/typescript/lib/lib.esnext.d.ts'],
+    },
 };
 
 export default nextConfig;
