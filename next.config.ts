5a65a4d3d07a5882db0448578e96eefaaabd6343
Biboswan Roy <biboswan98@gmail.com>
2025-10-26 21:17:33 +0530

    add only specific typescript files needed in the next config to be included

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   
     outputFileTracingIncludes: {
-      '/api/generate-lesson': ['./node_modules/typescript/lib/**'],
+      '/api/generate-lesson': ['./node_modules/typescript/lib/lib.esnext.d.ts', './node_modules/typescript/lib/lib.dom.d.ts'],
     },
 };
 
